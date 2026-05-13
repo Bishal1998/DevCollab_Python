e88d545c9192598d8ccf706ab04c98af9085fda9
Bishal Paudel <bishalpaudel1998@gmail.com>
2026-05-12 21:43:23 -0400

    User model created

---
+++ app/model/User.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from typing import Optional
+from uuid import UUID, uuid4
+
+from pydantic import EmailStr
+from sqlalchemy.dialects import postgresql
+from sqlmodel import Column, Field, SQLModel
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+
+    id: UUID = Field(sa_column=Column(postgresql.UUID, primary_key=True, default=uuid4))
+    email: EmailStr = Field(
+        sa_column=Column(postgresql.VARCHAR, unique=True, nullable=False)
+    )
+    hashed_password: str = Field(nullable=False)
+    name: str = Field(nullable=False)
+    created_at: datetime = Field(
+        sa_column=Column(postgresql.TIMESTAMP, default=datetime.now)
+    )
+    updated_at: Optional[datetime] = Field(
+        sa_column=Column(
+            postgresql.TIMESTAMP, default=None, onupdate=datetime.now, nullable=True
+        )
+    )
+    deleted_at: Optional[datetime] = Field(
+        sa_column=Column(postgresql.TIMESTAMP, default=None, nullable=True)
+    )
