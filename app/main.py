8cff1d0dfdff1583b6e4eb4e0e147f6c880f39ea
Bishal Paudel <bishalpaudel1998@gmail.com>
2026-05-16 10:22:14 -0400

    main file updated with asynccontextmanager

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.database.session import create_db_and_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
 def root():
-    return {"detail" : "Welcome to DevCollab!"}
+    return {"detail": "Welcome to DevCollab!"}
