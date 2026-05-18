b7a4cca1ca41b01600d87059800b2ed882404093
Bishal Paudel <bishalpaudel1998@gmail.com>
2026-05-18 07:13:39 -0400

    get_all_members logic completed

--- app/services/project_member_service.py
+++ app/services/project_member_service.py
@@ -1,8 +1,11 @@
 from uuid import UUID
 
+from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlmodel import select
 
-from app.model.project_member import ProjectMemberRole
+from app.model.project import Project
+from app.model.project_member import ProjectMember, ProjectMemberRole
 from app.schema import InviteMember
 
 
@@ -11,7 +14,20 @@ class ProjectMemberService:
         self.session = session
 
     async def get_all_members(self, project_id: UUID):
-        pass
+        project = await self.session.get(Project, project_id)
+
+        if not project or project.deleted_at is not None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Project with id: {project_id} not found.",
+            )
+
+        result = await self.session.scalars(
+            select(ProjectMember).where(ProjectMember.project_id == project_id)
+        )
+
+        members = result.all()
+        return members
 
     async def invite_by_email(self, project_id, invite_detail: InviteMember):
         pass
