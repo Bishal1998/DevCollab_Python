7ac8e54eafa35fb9a876b04bbffa7237acf952b7
Bishal Paudel <bishalpaudel1998@gmail.com>
2026-05-26 20:41:25 -0400

    plan service updated

--- app/services/plan_service.py
+++ app/services/plan_service.py
@@ -1,9 +1,17 @@
+from uuid import UUID
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.model import Plan
+from app.services.base_service import BaseService
 
-class PlanService:
+
+class PlanService(BaseService):
     def __init__(self, session: AsyncSession):
-        self.session = session
+        super().__init__(Plan, session)
 
     async def get_all_plans(self):
-        pass
+        return await self._get_all()
+
+    async def get_plan(self, plan_id: UUID):
+        return await self._get(plan_id)
